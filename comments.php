d2d7532f1a88ddf5ea3c6fd27158bed034419631
Fernando <ffmp777@yahoo.com>
2016-06-10 15:16:15 -0600

    first commit

---
+++ comments.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+
+//This is not going to show.
+    echo "Hello Fernando."
+
+     ?>
+
+    <h1><?php echo $title; ?></h1>
+
+  </body>
+</html>
---
+++ dynamicText.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+
+    $title = "Text in PHP."
+
+     ?>
+
+    <h1><?php echo $title; ?></h1>
+
+  </body>
+</html>
