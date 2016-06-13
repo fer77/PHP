5e6283803facf709511712579ce9e26dd305aa83
Fernando <ffmp777@yahoo.com>
2016-06-13 08:40:57 -0600

    variables

--- comments.php
+++ comments.php
@@ -13,7 +13,5 @@
 
      ?>
 
-    <h1><?php echo $title; ?></h1>
-
   </body>
 </html>
---
+++ variables.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Variables</title>
+  </head>
+  <body>
+
+    <?php
+
+    //variables are declared with a $ followed with the name of the variable.
+    $name = "Fernando";
+    $number = 100;
+
+    echo $name;
+
+     ?>
+
+  </body>
+</html>
