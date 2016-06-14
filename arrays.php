a0dbbeb3caae02a2ce77c0ba9132c109a4860d38
Fernando <ffmp777@yahoo.com>
2016-06-14 08:19:21 -0600

    arrays

---
+++ arrays.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+
+    $numberList = ["Bob", "Linda", "Tina", "Gene", "loiuse", "<p>The Breakfast Club Burger</p>"];
+
+    print_r($numberList);
+
+    //echo $numberList[1];
+
+     ?>
+
+  </body>
+</html>
