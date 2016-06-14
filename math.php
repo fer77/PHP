0d2e8cf76dc61b098b9755d6cc4ca6d25ec419a4
Fernando <ffmp777@yahoo.com>
2016-06-14 08:10:14 -0600

    math

---
+++ math.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+
+    echo 56 + 45;
+    echo "<br/>";
+    echo 5 + 5 * 10;
+    echo "<br/>";
+    $number1 = 12;
+    $number2 = 24;
+    echo $number1 + $number2;
+
+     ?>
+
+  </body>
+</html>
