ee34583d2a9a8abfc36687096dc37f4f41411d7c
Fernando <ffmp777@yahoo.com>
2016-06-15 08:57:50 -0600

    comperison operators

---
+++ comperison_operators.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+
+    /*
+    COMPERISON OPERATORS:
+    equal ==
+    identical ===
+    compare > < >= <= <>
+    not equal !=
+    not identical !==
+    and &&
+    or ||
+    not !
+    */
+
+    if (4 === "4" || 5 < 10) {
+
+      echo "It is true!";
+
+    }
+
+     ?>
+
+  </body>
+</html>
