621bda8c90783caa9e227daa8ecd333065d68275
Fernando <ffmp777@yahoo.com>
2016-06-15 09:24:19 -0600

    switch

---
+++ switchStatements.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+
+//Great for testing one condition agains multiple difrent values (forms, entryies to DBs...).
+
+    $number = 24;
+
+    switch ($number) {
+      case 34:
+        echo "It is 34!!.";
+        break;
+
+      default:
+        echo "It is not 34.";
+        break;
+    }
+
+     ?>
+
+  </body>
+</html>
