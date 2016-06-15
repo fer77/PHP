1ac2ee7bf6d9bb7f514d076442b550cf17792bb4
Fernando <ffmp777@yahoo.com>
2016-06-15 08:45:59 -0600

    if statements

---
+++ ifStatement.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+
+    if (3 > 10) {
+
+      echo "Three is less than ten.";
+
+    } elseif(4 > 5) {
+
+      echo "Four is less than five.";
+
+    } else {
+
+      echo "It is not.";
+
+    }
+
+     ?>
+
+  </body>
+</html>
